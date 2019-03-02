c1a39d2a52ddf00f026a2b3f87a990b01fafd86b
Jae-eun <je4297@naver.com>
2019-03-02 19:27:33 +0900

    xunique

--- ConcetrationCardGame/ViewController.swift
+++ ConcetrationCardGame/ViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
   
+  lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
   var flipCount = 0 {
     didSet {
       flipCountLabel.text = "Flips: \(flipCount)"
     }
   }
   
-  var emojiChoices = ["👻","🎃","👻","🎃"]
   @IBOutlet var cardButtons: [UIButton]!
-  @IBOutlet weak var flipCountLabel:
-  UILabel!
+  @IBOutlet weak var flipCountLabel: UILabel!
   
   @IBAction func touchButton(_ sender: UIButton) {
     flipCount += 1
     if let cardNumber = cardButtons.index(of: sender) {
-      flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+      game.chooseCard(at: cardNumber)
     } else {
       print("chosen card was not in cardButtons")
     }
@@ -50,6 +49,16 @@ class ViewController: UIViewController {
     }
   }
   
+  var emojiChoices = ["👻", "🎃", "☠️", "🦇", "🍬", "🍭", "😱"]
+  
+  var emoji = [Int: String]()
+  
+  func emoji(for card: Card) -> String {
+    if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+      let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+      emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+    }
+    return emoji[card.identifier] ?? "?"
+  }
   
 }
-
